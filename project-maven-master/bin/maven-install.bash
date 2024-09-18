f6abbe41e5da440b4203ca6cb9c3c00793be6b43
San9Bumanskyi <148449855+San9Bumanskyi@users.noreply.github.com>
2024-09-18 20:37:06 +0300

    Add files via upload

---
+++ project-maven-master/bin/maven-install.bash
@@ -0,0 +1,1 @@
+mvn install:install-file -Dfile=D:\JAVAR\Projects\project-maven-master\lib\desktop-game-engine.jar -DgroupId=com.javarush -DartifactId=desktop-game-engine -Dversion=1.0 -Dpackaging=jar
